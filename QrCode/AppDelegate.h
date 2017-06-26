3c24bcc72ae9e0318d6ebda25b83e00a9d313247
AZName <964190389@qq.com>
2017-06-26 14:08:52 +0800

    Initial Commit

---
+++ QrCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QrCode
+//
+//  Created by 徐振 on 2017/6/26.
+//  Copyright © 2017年 徐振. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
